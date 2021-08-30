a99953012479f62fe22496fb7b99bb18c839497c
Luelen Cavalheiro <87206932+luelencavalheiro@users.noreply.github.com>
2021-08-30 20:57:52 -0300

    Adicionado enunciado e resolução exercício 18

---
+++ exercicio-18/README.md
@@ -0,0 +1,10 @@
+<div class="layout-pane__container"><div id="main-splitpane-left" class="coding-question__left-pane"><section class="question-view__title-wrapper"><h1 class="question-view__title">18. Operadores Lógicos - AND</h1></section><section class="question-view__instruction"><div class="candidate-rich-text"><div id="5d4s497ihtg-instruction"><p>Complete o código deste exercício atribuindo, na variável <code>resultado</code>, a seguinte comparação: variável <code>elementoA</code>&nbsp;<strong>E</strong> a variável&nbsp;<code>elementoB</code></p>
+
+<p>&nbsp;</p>
+
+<p>Obs: Neste exercício, todas as variáveis necessárias já estão declaradas, você não precisa cria-las, apenas completar o código.&nbsp;</p>
+</div></div></section></div></div>
+
+____
+
+[Resolução AQUI](https://github.com/luelencavalheiro/curso-introdutorio-javascript/blob/main/exercicio-18/resolucao.js)
---
+++ exercicio-18/resolucao.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// start main function
+function comparacaoEntreElementos(elementoA, elementoB) {
+// não altere a linha acima
+    
+/*
+ * Complete a função atribuindo na 
+ * variável `resultado` a comparação 
+ * `elementoA` E `elementoB`.
+ */
+  
+  var resultado = elementoA && elementoB;
+
+// não altere as linhas abaixo
+  return resultado;
+}
+// fim do seu código
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  
+    const inputs = readLine().split(' ');
+
+    const result = comparacaoEntreElementos(inputs[0] === 'true', inputs[1] === 'true');
+  
+    ws.write(result + '\n');
+  
+    ws.end();
+}
