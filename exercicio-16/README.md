36c69235a5f21c05133ed80b81fd449d94fd0356
Luelen Cavalheiro <87206932+luelencavalheiro@users.noreply.github.com>
2021-08-30 20:55:34 -0300

    Adicionado enunciado e resolução exercício 16

---
+++ exercicio-16/README.md
@@ -0,0 +1,10 @@
+<div class="layout-pane__container"><div id="main-splitpane-left" class="coding-question__left-pane"><section class="question-view__title-wrapper"><h1 class="question-view__title">16. Operadores de Comparação - Igualdade</h1></section><section class="question-view__instruction"><div class="candidate-rich-text"><div id="4qbt8k7gba6-instruction"><p>Complete o código deste exercício atribuindo, na variável&nbsp;<code>resultado</code>, a seguinte verificação: variável&nbsp;<code>elementoA</code>&nbsp;<u><strong>É IGUAL A</strong></u>&nbsp;variável&nbsp;<code>elementoB</code>?</p>
+
+<p>&nbsp;</p>
+
+<p>Obs: Neste exercício, as variáveis <code>resultado</code>,&nbsp;<code>elementoA</code>&nbsp;e&nbsp;<code>elementoB</code>&nbsp;já existem, você não precisa cria-las, apenas completar o código.&nbsp;</p>
+</div></div></section></div></div>
+
+____
+
+[Resolução AQUI](https://github.com/luelencavalheiro/curso-introdutorio-javascript/blob/main/exercicio-16/resolucao.js)
---
+++ exercicio-16/resolucao.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// start main function
+function comparacaoEntreElementos(elementoA, elementoB) {
+// não altere a linha acima
+    
+/*
+ * Atribua como valor da variável `resultado` a verificação:
+ * `elementoA` igual a `elementoB`?
+ */
+  
+  let resultado = elementoA === elementoB;
+
+// não altere as linhas abaixo
+  return resultado;
+}
+// fim do seu código
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  
+    const inputs = readLine().split(' ');
+
+    const result = comparacaoEntreElementos(inputs[0], inputs[1]);
+  
+    ws.write(result + '\n');
+  
+    ws.end();
+}
